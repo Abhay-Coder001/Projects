f4988d7e7f96353ea6300e00ae65d7aedb40c580
Abhay-Coder001 <djoker682@gmail.com>
2022-05-19 18:56:45 +0530

    First commit

---
+++ GeneratingPseudonyms.py
@@ -0,0 +1,8 @@
+# Project 1 : Generating Pseudonyms
+
+"""In this project we will write a simple Python program that generates silly
+names by randomly combining first names and surnames. With any luck, you’ll produce
+a plethora of aliases that would make any sidekick proud. You’ll also review best­
+practice coding guidelines and apply external programs that will help you write code
+that conforms to those guidelines.
+"""
